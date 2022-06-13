3e53f293a1fad07db803bf7f2bdfd0945c756bf4
Yana Hrynevich <86208169+yanagriw@users.noreply.github.com>
2022-06-13 21:04:37 +0200

    Update diabetes.py

--- Diabetes prediction with Scikit-learn/diabetes.py
+++ Diabetes prediction with Scikit-learn/diabetes.py
@@ -24,5 +24,5 @@ print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 print("Precision:", metrics.precision_score(y_test, y_pred))
 print("Recall:", metrics.recall_score(y_test, y_pred))
 
-# tree.plot_tree(dec_tree, feature_names=feature_columns, filled=True)
-# plt.show()
+tree.plot_tree(dec_tree, feature_names=feature_columns, filled=True)
+plt.show()
